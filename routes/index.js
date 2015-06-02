1c51d26290d4c91172cb9e8a52a5c21c80e33318
Joaquin Gonzalez Santos <contacto@joaquinglezsantos.es>
2015-06-02 15:25:44 +0100

    Entrega P2P Modulo 6

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz 2015' });
 });
 
+router.get('/author', function(req, res, next) {
+  res.render('author', {authors: [{ name: 'Joaquín González Santos', urlphoto: '/images/author.png', urlvideo: 'http://www.html5videoplayer.net/videos/toystory.mp4' }, { name: 'Autor 2', urlphoto: '/images/author_default.png' }]});
+});
+
 //GESTION DE QUIZES
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
