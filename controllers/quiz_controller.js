5fa92061742b110d892f598a81c39c93dff14eb9
joaquinglezsantos <contacto@joaquinglezsantos>
2015-06-02 11:50:27 +0100

    Primera pregunta

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,9 @@
+﻿// GET /quizes/question
+exports.question = function(req, res) {
+	res.render('quizes/question', {pregunta: 'Capital de Italia'});
+}
+
+// GET /quizes/answer
+exports.answer = function(req, res) {
+	res.render('quizes/answer', {respuesta: req.query.respuesta.toUpperCase() === 'Roma'.toUpperCase() ? true : false});
+}
