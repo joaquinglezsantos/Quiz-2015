59fab90da2b6c0a1843c14772f4fc0712bfad0da
Joaquin Gonzalez Santos <contacto@joaquinglezsantos.es>
2015-06-10 15:10:25 +0100

    Implementación SQLite

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,9 +1,17 @@
-﻿// GET /quizes/question
+﻿var models = require('../models/models.js');
+
+// GET /quizes/question
 exports.question = function(req, res) {
-	res.render('quizes/question', {pregunta: 'Capital de Italia'});
+	//res.render('quizes/question', {pregunta: 'Capital de Italia'});
+	models.Quiz.findAll().then(function(quiz) {
+		res.render('quizes/question', {pregunta: quiz[0].pregunta});
+	});
 }
 
 // GET /quizes/answer
 exports.answer = function(req, res) {
-	res.render('quizes/answer', {respuesta: req.query.respuesta.toUpperCase() === 'Roma'.toUpperCase() ? true : false});
+	//res.render('quizes/answer', {respuesta: req.query.respuesta.toUpperCase() === 'Roma'.toUpperCase() ? true : false});
+	models.Quiz.findAll().then(function(quiz) {
+		res.render('quizes/answer', {respuesta: req.query.respuesta.toUpperCase() === quiz[0].respuesta.toUpperCase() ? true : false});
+	});
 }
