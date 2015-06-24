b1c05b8ae12e0efe1ebde23aa4b733c75564b4de
Joaquin Gonzalez Santos <contacto@joaquinglezsantos.es>
2015-06-24 08:34:47 +0100

    Estadisticas

---
+++ controllers/estadistic_controller.js
@@ -0,0 +1,40 @@
+﻿var models = require('../models/models.js');
+
+exports.index = function(req, res, next) {
+	models.Quiz.findAll({
+		include: [{ model: models.Comment }] 
+	}).then(function(quizes) {
+		var numPreguntas = quizes.length || 0;
+		var numComentariosTotal = 0;
+		var numComentariosPregunta = 0;
+		var numPreguntasConComentario = 0;
+		var numPreguntasSinComentario = 0;
+		var mediaComentario = 0;
+		for (var i = 0; i < numPreguntas; i++) {
+			numComentariosPregunta = 0;
+			for(j in quizes[i].Comments) {
+				numComentariosTotal += 1;
+				numComentariosPregunta += 1;
+			}
+			if(numComentariosPregunta > 0) { 
+				numPreguntasConComentario += 1;
+			}
+			else {
+				numPreguntasSinComentario += 1;
+			}
+		}
+		if(numPreguntas > 0) {
+			mediaComentario = (numComentariosTotal / numPreguntas).toFixed(2); 
+		}
+		res.render('estadistics', {
+			numPreguntas :numPreguntas,
+			numComentariosTotal: numComentariosTotal,
+			numPreguntasSinComentario: numPreguntasSinComentario,
+			numPreguntasConComentario: numPreguntasConComentario,
+			mediaComentario: mediaComentario,
+			errors: []
+		});    
+	}).catch(function(err) {
+		next(err);
+	})
+};
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
 var sessionController = require('../controllers/session_controller');
+var estadisticController = require('../controllers/estadistic_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -36,5 +37,8 @@ router.delete('/quizes/:quizId(\\d+)', 		sessionController.loginRequired, quizCo
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
 router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
+
+//GESTION DE ESTADISTICS
+router.get('/quizes/statistics', estadisticController.index);
 
 module.exports = router;
